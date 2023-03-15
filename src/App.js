9d72c5e69f459aa5d3a130d92fbd92749fc561bc
muzaahmadk2 <muzaahmadk2@gmail.com>
2023-03-15 21:38:51 +0400

    making api calls in react

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading,setIsLoading] = useState(false)
+
+  async function movieHandler() {
+    setIsLoading(true);
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+    const transformedMovies = data.results.map((movie) => {
+      return {
+        id: movie.episode_id,
+        title: movie.title,
+        openingText: movie.opening_crawl,
+        releaseDate: movie.release_date,
+      };
+    });
+    setMovies(transformedMovies);
+    setIsLoading(false);
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={movieHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {!isLoading && <MoviesList movies={movies} />}
+        {isLoading && <p>Loading....</p>}
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
