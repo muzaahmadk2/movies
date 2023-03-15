88d793b0068c3e0c592ea7309773ad76c79d9cbe
muzaahmadk2 <muzaahmadk2@gmail.com>
2023-03-15 22:39:00 +0400

    making app error proof

--- src/App.js
+++ src/App.js
@@ -5,22 +5,45 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [isLoading,setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setErro] = useState(null);
 
   async function movieHandler() {
     setIsLoading(true);
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
-    const transformedMovies = data.results.map((movie) => {
-      return {
-        id: movie.episode_id,
-        title: movie.title,
-        openingText: movie.opening_crawl,
-        releaseDate: movie.release_date,
-      };
-    });
-    setMovies(transformedMovies);
+    setErro(null);
+
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      if (!response.ok) {
+        throw new Error("Something went wrong .....");
+      }
+      const data = await response.json();
+      const transformedMovies = data.results.map((movie) => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          openingText: movie.opening_crawl,
+          releaseDate: movie.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setErro(error.message);
+    }
+
     setIsLoading(false);
+  }
+
+  let content = <p>FOund no Movies</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+  if (error) {
+    content = <p>{error}</p>;
+  }
+  if (isLoading) {
+    content = <p>Loading.....</p>;
   }
 
   return (
@@ -28,10 +51,7 @@ function App() {
       <section>
         <button onClick={movieHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && <MoviesList movies={movies} />}
-        {isLoading && <p>Loading....</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
