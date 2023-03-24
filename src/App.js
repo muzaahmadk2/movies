dfef2d35fb3ede2270b33993091b1faaaef17c99
muzaahmadk2 <muzaahmadk2@gmail.com>
2023-03-24 21:59:28 +0400

    post request

--- src/App.js
+++ src/App.js
@@ -14,19 +14,23 @@ function App() {
     setErro(null);
 
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch(
+        "https://reacr-http-82765-default-rtdb.firebaseio.com/Movies.json "
+      );
       if (!response.ok) {
         throw new Error("Something went wrong .....");
       }
       const data = await response.json();
-      const transformedMovies = data.results.map((movie) => {
-        return {
-          id: movie.episode_id,
-          title: movie.title,
-          openingText: movie.opening_crawl,
-          releaseDate: movie.release_date,
-        };
-      });
+      const transformedMovies = [];
+      for (const key in data) {
+        transformedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
       setMovies(transformedMovies);
     } catch (error) {
       setErro(error.message);
@@ -39,14 +43,25 @@ function App() {
     movieHandler();
   }, [movieHandler]);
 
-  const addMovieHandler = (movie) => {
-    console.log(movie);
-  };
+  async function addMovieHandler(movie) {
+    const response = await fetch(
+      "https://reacr-http-82765-default-rtdb.firebaseio.com/Movies.json ",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+  }
 
   let content = <p>FOund no Movies</p>;
 
   if (movies.length > 0) {
-    content = <MoviesList movies={movies} />;
+    content = <MoviesList movies={movies}/>;
   }
   if (error) {
     content = <p>{error}</p>;
